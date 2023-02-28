278832677c2ea016a0d88a4055a883f1ac4c52c1
Habibe_from_Sliit <116754374+avis1999@users.noreply.github.com>
2023-02-28 10:23:39 +0530

    Create custom_encanddec_for_usertiers.py

---
+++ custom_encanddec_for_usertiers.py
@@ -0,0 +1,89 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import pycuda.gpuarray as gpuarray
+import numpy as np
+from Crypto.Cipher import AES
+
+# Define key and data
+key = '0123456789abcdef0123456789abcdef'
+data = 'This is a test message'
+
+# Initialize AES cipher
+cipher = AES.new(key, AES.MODE_ECB)
+
+# Convert data to bytes
+data_bytes = bytes(data, 'utf-8')
+
+# Pad data to multiple of 16 bytes
+data_bytes_padded = data_bytes + b'\\\\0' * (16 - len(data_bytes) % 16)
+
+# Encrypt data
+encrypted_data = cipher.encrypt(data_bytes_padded)
+
+# Convert encrypted data to GPU array
+encrypted_data_gpu = gpuarray.to_gpu(np.frombuffer(encrypted_data, dtype=np.byte))
+
+# Define CUDA kernel for AES decryption
+mod = SourceModule("""__global__ void aes_decrypt(unsigned char *input, unsigned char *output, int length, unsigned char *key) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < length) {
+        AES_KEY dec_key;
+        AES_set_decrypt_key(key, 256, &dec_key);
+        AES_decrypt(input + idx, output + idx, &dec_key);
+    }
+}""")
+
+# Define CUDA kernel function
+aes_decrypt = mod.get_function("aes_decrypt")
+
+# Define tiers for custom configuration
+tiers = [
+    {'parallelism': True, 'block_size': 256, 'encryption_mode': 'ECB'},
+    {'parallelism': True, 'block_size': 512, 'encryption_mode': 'CBC'},
+    {'parallelism': False, 'block_size': 1024, 'encryption_mode': 'CBC'}
+]
+
+# Decrypt data for each tier
+for i, tier in enumerate(tiers):
+    print(f'Tier {i + 1}:')
+
+    # Define CUDA kernel function
+    if tier['parallelism']:
+        mod = SourceModule(f"""__global__ void aes_decrypt(unsigned char *input, unsigned char *output, int length, unsigned char *key) {{
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx < length) {{
+                AES_KEY dec_key;
+                AES_set_decrypt_key(key, 256, &dec_key);
+                AES_decrypt(input + idx, output + idx, &dec_key);
+            }}
+        }}""")
+        aes_decrypt = mod.get_function("aes_decrypt")
+
+    # Decrypt data
+    decrypted_data_gpu = gpuarray.empty_like(encrypted_data_gpu)
+    aes_decrypt(
+        encrypted_data_gpu,
+        decrypted_data_gpu,
+        np.int32(len(encrypted_data)),
+        np.frombuffer(bytes(key, 'utf-8'), dtype=np.byte),
+        block=(tier['block_size'], 1, 1),
+        grid=((len(encrypted_data) + tier['block_size'] - 1) // tier['block_size'], 1, 1)
+    )
+
+    # Convert decrypted data to bytes
+    decrypted_data = decrypted_data_gpu.get()
+    decrypted_data_bytes = bytes(decrypted_data)
+
+    # Remove padding from decrypted data
+    decrypted_data_bytes_unpadded = decrypted_data_bytes.rstrip(b'\\\\0')
+
+    # Convert decrypted data to string
+    decrypted_data_str = decrypted_data_bytes_unpadded.decode('utf-8')
+
+    # Print decrypted data and performance metrics
+    print(decrypted_data_str)
+    print(f'Block size: {tier["block_size"]}')
+    print(f'Encryption mode: {tier["encryption_mode"]}')
+    print(f'Parallelism: {str(tier["parallelism"]).lower()}')
+    print('\\\\n')
